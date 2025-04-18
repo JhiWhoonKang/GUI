79fee26fee5da17af3ca9c7d7baaf38935a0101f
JhiWhoonKang <kangjw0914@naver.com>
2025-04-19 01:23:20 +0900

    Add Echo Server/Client with TCP/IP

---
+++ WinForm/EchoServer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace EchoServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LV_SERVER = new System.Windows.Forms.ListView();
+            this.BTN_STOP = new System.Windows.Forms.Button();
+            this.BTN_OPEN = new System.Windows.Forms.Button();
+            this.BTN_EXIT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LV_SERVER
+            // 
+            this.LV_SERVER.HideSelection = false;
+            this.LV_SERVER.Location = new System.Drawing.Point(17, 18);
+            this.LV_SERVER.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LV_SERVER.Name = "LV_SERVER";
+            this.LV_SERVER.Size = new System.Drawing.Size(561, 637);
+            this.LV_SERVER.TabIndex = 0;
+            this.LV_SERVER.UseCompatibleStateImageBehavior = false;
+            // 
+            // BTN_STOP
+            // 
+            this.BTN_STOP.Location = new System.Drawing.Point(589, 88);
+            this.BTN_STOP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BTN_STOP.Name = "BTN_STOP";
+            this.BTN_STOP.Size = new System.Drawing.Size(139, 62);
+            this.BTN_STOP.TabIndex = 1;
+            this.BTN_STOP.Text = "Stop";
+            this.BTN_STOP.UseVisualStyleBackColor = true;
+            this.BTN_STOP.Click += new System.EventHandler(this.BTN_STOP_Click);
+            // 
+            // BTN_OPEN
+            // 
+            this.BTN_OPEN.Location = new System.Drawing.Point(589, 18);
+            this.BTN_OPEN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BTN_OPEN.Name = "BTN_OPEN";
+            this.BTN_OPEN.Size = new System.Drawing.Size(139, 62);
+            this.BTN_OPEN.TabIndex = 2;
+            this.BTN_OPEN.Text = "Open";
+            this.BTN_OPEN.UseVisualStyleBackColor = true;
+            this.BTN_OPEN.Click += new System.EventHandler(this.BTN_OPEN_Click);
+            // 
+            // BTN_EXIT
+            // 
+            this.BTN_EXIT.Location = new System.Drawing.Point(589, 159);
+            this.BTN_EXIT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BTN_EXIT.Name = "BTN_EXIT";
+            this.BTN_EXIT.Size = new System.Drawing.Size(139, 62);
+            this.BTN_EXIT.TabIndex = 3;
+            this.BTN_EXIT.Text = "Exit";
+            this.BTN_EXIT.UseVisualStyleBackColor = true;
+            this.BTN_EXIT.Click += new System.EventHandler(this.BTN_EXIT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 675);
+            this.Controls.Add(this.BTN_EXIT);
+            this.Controls.Add(this.BTN_OPEN);
+            this.Controls.Add(this.BTN_STOP);
+            this.Controls.Add(this.LV_SERVER);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "EchoServer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView LV_SERVER;
+        private System.Windows.Forms.Button BTN_STOP;
+        private System.Windows.Forms.Button BTN_OPEN;
+        private System.Windows.Forms.Button BTN_EXIT;
+    }
+}
+
---
+++ WinForm/EchoServer/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace EchoServer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener tcpListener;
+        private TcpClient connectedClient;
+        private bool isRunning = false;
+        private Thread listenerThread;
+        private const int Port = 9000;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LVInit();
+        }
+
+        private void LVInit()
+        {
+            LV_SERVER.View = View.Details;
+            LV_SERVER.Columns.Add("시간", 100, HorizontalAlignment.Left);
+            LV_SERVER.Columns.Add("데이터", 300, HorizontalAlignment.Left);
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, Port);
+                tcpListener.Start();
+
+                while (isRunning)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    connectedClient = client;
+
+                    Invoke(new Action(() =>
+                    {
+                        string remoteEP = client.Client.RemoteEndPoint.ToString();
+                        AddListViewItem(DateTime.Now.ToString("HH:mm:ss"), $"연결 됐수다: {remoteEP}");
+                    }));
+
+                    Thread clientThread = new Thread(() => ControlClient(client));
+                    clientThread.IsBackground = true;
+                    clientThread.Start();
+                }
+            }
+
+            catch (SocketException ex)
+            {
+                Invoke(new Action(() =>
+                {
+                    AddListViewItem("서버 ㅂㅂ", ex.Message);
+                }));
+            }
+        }
+
+        private void ControlClient(TcpClient client)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+
+                // Running flag, client 연결 확인 하고 ㄱㄱ
+                while (isRunning)
+                {
+                    int bytesRead = 0;
+                    try
+                    {
+                        bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    Invoke(new Action(() =>
+                    {
+                        AddListViewItem(DateTime.Now.ToString("HH:mm:ss"), receivedData);
+                    }));
+                }
+
+                Invoke(new Action(() =>
+                {
+                    AddListViewItem(DateTime.Now.ToString("HH:mm:ss"), "클라이언트 ㅂㅂ");
+                }));
+
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() =>
+                {
+                    AddListViewItem("클라이언트 에러 ;;", ex.Message);
+                }));
+            }
+
+            finally
+            {
+                Socket_Close(client);
+            }
+        }
+
+        private void AddListViewItem(string time, string data)
+        {
+            ListViewItem item = new ListViewItem(time);
+            item.SubItems.Add(data);
+            LV_SERVER.Items.Add(item);
+        }
+
+        private void BTN_OPEN_Click(object sender, EventArgs e)
+        {            
+            if (!isRunning)
+            {
+                Btn_Status_Open();
+
+                listenerThread = new Thread(new ThreadStart(StartServer));
+                listenerThread.IsBackground = true;
+                listenerThread.Start();
+                AddListViewItem("서버 ㄱㄱ", $"포트 {Port}");
+            }
+        }
+
+        private void BTN_STOP_Click(object sender, EventArgs e)
+        {
+            Btn_Status_Close();
+            tcpListener?.Stop();
+        }
+
+        private void BTN_EXIT_Click(object sender, EventArgs e)
+        {
+            Socket_Close();
+            Application.Exit();
+        }
+
+        private void Btn_Status_Open()
+        {
+            BTN_OPEN.Enabled = false;
+            BTN_STOP.Enabled = true;
+            isRunning = true;
+        }
+
+        private void Btn_Status_Close()
+        {
+            BTN_OPEN.Enabled = true;
+            BTN_STOP.Enabled = false;
+            isRunning = false;
+        }
+
+        private void Socket_Close()
+        {
+            connectedClient?.GetStream().Close();
+            connectedClient?.Close();
+            connectedClient = null;
+        }
+
+        private void Socket_Close(TcpClient client)
+        {
+            if (connectedClient == client)
+            {
+                connectedClient = null;
+            }
+
+            client?.GetStream().Close();
+            client?.Close();
+            client = null;
+        }
+    }
+}
